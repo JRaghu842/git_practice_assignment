a01df7c372c9f23fe583c1b7fad55aba0d753481
ak1code <akshay.ramoliya9@gmail.com>
2022-11-30 13:26:04 +0530

    assignment completed

--- primenumber.js
+++ primenumber.js
@@ -3,13 +3,16 @@
 //Check if the number is a prime number or not
 //If the value stored in num, is a prime number print Yes, else print No
 
-    let num = 10
+   let num = 11;
     let count =0;
-    for (let i=0; i<num; i++) {
-        if (i%num == 0) {
+    for (let i=0; i<=num; i++) {
+        if (num%i == 0) {
             count++
         }
     }
 
-    if (count>2) {
-        console.log("Yes"){
+    if (count==2) {
+        console.log("Yes")}
+          else{
+      console.log("No")
+          }
